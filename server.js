00d9e1dc5efea0722c127b487b228430efbecddb
EngrVictor <victorjnr300@gmail.com>
2020-06-15 17:56:53 +0100

    updated files

--- server.js
+++ server.js
@@ -1,63 +1,68 @@
 
+'use strict';
 
+// ================================================================
+// get all the tools we need
+// ================================================================
 var express = require('express');
+// var routes = require('./routes.js');
+var port = process.env.PORT || 3000;
+
 var app = express();
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var path = require('path'); //for 
 
-var quoteSchema = mongoose.Schema({
-	author: String,
-	created_at : {type: Date , default:Date.now()},
-	body: String,
-	title: String
+// ================================================================
+// setup our express application
+// ================================================================
+app.use('/assets', express.static(process.cwd() + '/assets'));
+app.set('view engine', 'ejs');
+
+
+// ================================================================
+// setup routes
+// ================================================================
+// routes(app);
+
+// ================================================================
+// start our server
+// ================================================================
+
+const contentful = require('contentful')
+
+let client = contentful.createClient({
+  space: 'xai0al1ows0s',
+  accessToken: 'O4iy74XCWHyoxSivUqhJkudOfKU0RdQTMzbsIsVWshY'
+})
+
+let entryId = '651CQ8rLoIYCeY6G0QG22q';
+let entryId2 = '7LAnCobuuWYSqks6wAwY2a';
+
+client.getEntry(entryId)
+.then((entry) => console.log(entry))
+.catch(console.error)
+
+client.getEntry(entryId2)
+.then((entry) => console.log(entry))
+.catch(console.error)
+
+app.get('/', function(req, res) {
+        res.render('pages/index', {'result': {
+          ProductName: 'name',
+          age: 32,
+          job: 'software developer',
+        }
+        });
+    });
+
+    app.get('/product', function(req, res) {
+        res.render('pages/product', {'result': {
+          ProductName: 'name',
+          age: 32,
+          job: 'software developer',
+        }
+        });
+    });
+
+
+app.listen(port, function() {
+    console.log('Server listening on port ' + port + '...');
 });
-
-let quoteModel = mongoose.model('quoteModel', quoteSchema);
-
-module.exports = quoteModel;
-
-mongoose.connect('mongodb://ram:fakepass1@ds157742.mlab.com:57742/quoteapp');
-
-app.use(express.static(__dirname + '/public'));
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/public/quotes/quotes.html'));
-});
-
-app.get('/viewquotes',function(req,res){
-      res.sendFile(path.join(__dirname+'/public/viewquotes/viewquotes.html'));
-});
-
-app.post('/post',function(req,res){
-  var newQuote = new quote();
-  newQuote.title = req.body.title;
-  newQuote.body = req.body.body;
-  newQuote.author = req.body.author;
-  newQuote.save(function(err,savedObject){
-      if(savedObject)
-      {
-        res.redirect('/viewquotes')
-      }
-      else {
-        res.send(err);
-      }
-});
-});
-
-app.get('/get/quotes',function(req,res){
-  quote.find({},function(err,data){
-    res.send(data);
-  })
-});
-
-app.post('/delete/:id/quote',function(req,res){
-  quote.findOneAndDelete({_id: req.params.id},function(deleteditem){
-    res.redirect('/viewquotes')
-  });
-});
-
-app.listen(8080);
