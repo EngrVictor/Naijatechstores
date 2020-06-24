55cf9eb38e24d1028b6bcada394176895d0d9b0e
EngrVictor <victorjnr300@gmail.com>
2020-06-24 11:18:10 +0100

    updated server file

--- server.js
+++ server.js
@@ -26,39 +26,39 @@ app.set('view engine', 'ejs');
 // start our server
 // ================================================================
 
-// const contentful = require('contentful')
+const contentful = require('contentful')
 
-// let client = contentful.createClient({
-//   space: '506rx1s4kvpq',
-//   accessToken: 'cpYUmV3UQjbOMBkY90P-CDkzE1cy5fpdhPdaEvfAn_g'
-// })
+let client = contentful.createClient({
+  space: '506rx1s4kvpq',
+  accessToken: 'cpYUmV3UQjbOMBkY90P-CDkzE1cy5fpdhPdaEvfAn_g'
+})
 
-// let entryId = '6Jk1gKk1upCKWfHikSRSyh';
+let entryId = '6Jk1gKk1upCKWfHikSRSyh';
 
-// client.getEntry(entryId)
-// .then((entry) => {
-//   start(entry);
-// })
-// .catch(console.error)
+client.getEntry(entryId)
+.then((entry) => {
+  start(entry);
+})
+.catch(console.error)
 
-// let priceSplit = given => {
-//   let price = given.toString().split('');
+let priceSplit = given => {
+  let price = given.toString().split('');
 
-//   if (price.length >= 4) {
-//     let newPrice = price.splice(-3);
-//     let currentPrice = price.join('') + ',' + newPrice.join('');
+  if (price.length >= 4) {
+    let newPrice = price.splice(-3);
+    let currentPrice = price.join('') + ',' + newPrice.join('');
 
-//     if (currentPrice.length > 7) {
-//       let current = currentPrice.toString().split('');
-//       let endPrice = current.splice(-7);
-//       currentPrice = current.join('') + ',' + endPrice.join('');
-//     }
-//     return currentPrice;
-//   }
-// }
+    if (currentPrice.length > 7) {
+      let current = currentPrice.toString().split('');
+      let endPrice = current.splice(-7);
+      currentPrice = current.join('') + ',' + endPrice.join('');
+    }
+    return currentPrice;
+  }
+}
 
-// let start = entry => {
-//   let newPrice = priceSplit(entry.fields.price);
+let start = entry => {
+  let newPrice = priceSplit(entry.fields.price);
 
   app.get('/product', function(req, res) {
     res.render('product', {'result': {
@@ -74,7 +74,7 @@ app.set('view engine', 'ejs');
       }
       });
   });
-// }
+}
 
 
 app.listen(port, function() {
